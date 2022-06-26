ef8dadbe328dff6d9e4c7ffcf83f4926c82dcb56
whistyun <kanadische_goldrute@yahoo.co.jp>
2022-06-26 19:23:29 +0900

    add benchmark project

---
+++ test/Html2Markdown.Benchmark/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.IO;
+
+namespace Html2Markdown.Benchmark
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+
+    public class Executor
+    {
+        private Converter _converter;
+        private string _markdown;
+
+        public Executor()
+        {
+            _converter = new Converter();
+
+            using (var stream = typeof(Program).Assembly.GetManifestResourceStream("Html2Markdown.Benchmark.TargetText.html"))
+            using (var reader = new StreamReader(stream))
+                _markdown = reader.ReadToEnd();
+        }
+
+        [Benchmark]
+        public string Convert()
+        {
+            return _converter.Convert(_markdown);
+        }
+    }
+}
